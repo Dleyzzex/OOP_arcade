81091037621c13c0930e6a49a53bc3d071d6c87f
QuentinP <quentin.peridon@epitech.eu>
2020-04-03 22:42:37 +0000

    ADD - Pacman game finished

--- games/Snake/src/Pacman.hpp
+++ games/Snake/src/Pacman.hpp
@@ -11,6 +11,8 @@
 
 #include <ncurses.h>
 #include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string>
 #include <iostream>
 #include <vector>
@@ -33,7 +35,17 @@ class Pacman {
         void InitFood(void);
         void InitPacman(void);
         void Update(void);
-
+        void witch_color(std::string str);
+        void Moving(void);
+        void nb_zero(int i);
+        void nb_one(int i);
+        void nb_two(int i);
+        void nb_three(int i);
+        int GetMove(int _direction);
+        bool CanMove(void);
+        bool doesittouch(int dir);
+        void MovePacman(void);
+        void gain_coin(void);
     protected:
     private:
         int _FoodRemaining;
@@ -43,6 +55,11 @@ class Pacman {
         std::map<int, std::pair<int, int>> _Ghost;
         std::map<int, std::pair<int, int>> _Food;
         std::map<int, std::pair<int, int>> _SuperFood;
+        int _direction;
+        int _powerup;
+        bool _can_go;
+        bool _can_move;
 };
+char *  itoa ( int value, char * str, int base );
 
 #endif /* !PACMAN_HPP_ */
